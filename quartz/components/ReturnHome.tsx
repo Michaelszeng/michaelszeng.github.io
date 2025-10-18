ca1e413ebfa2b6954bc1d7007624f89cb787be02
Michaelszeng <michaelszeng@gmail.com>
2025-10-18 15:45:42 -0400

    fix return to home behavior

--- quartz/components/ReturnHome.tsx
+++ quartz/components/ReturnHome.tsx
@@ -3,7 +3,7 @@ import { QuartzComponent, QuartzComponentConstructor } from "./types"
 export default (() => {
   const ReturnHome: QuartzComponent = () => (
     <nav class="breadcrumb-container" aria-label="breadcrumbs">
-      <a href="/index.html">← Return to Home</a>
+      <a href="/index.html" data-router-ignore>← Return to Home</a>
     </nav>
   )
 
